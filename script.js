e3f871acb6777de7887d29945bc686ce5998a499
Kinz <161678979+kaniz-naqvi@users.noreply.github.com>
2025-01-09 15:45:04 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,44 @@
-const URL='https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@{date}/{apiVersion}/{endpoint}';
+const toCurrency = document.getElementById("to-currency");
+countriesCurrencies.forEach((code) => {
+  let child = document.createElement("option");
+  child.value = code.currency;
+  child.textContent = code.country;
+  code.country == "PK" && (child.selected = true);
+  toCurrency.appendChild(child);
+});
+const fromCurrency = document.getElementById("from-currency");
+countriesCurrencies.forEach((code) => {
+  let child = document.createElement("option");
+  child.value = code.currency;
+  child.textContent = code.country;
+  code.country == "US" && (child.selected = true);
+  fromCurrency.appendChild(child);
+});
+let fromImg = document.querySelector(".label1 img");
+fromCurrency.addEventListener("change", (e) => {
+  let value = e.target.value;
+  let name = e.target.options[e.target.selectedIndex].textContent;
+  fromImg.src = `https://flagsapi.com/${name}/flat/64.png`;
+  convertCurrency();
+});
+let toImg = document.querySelector(".label2 img");
+toCurrency.addEventListener("change", (e) => {
+  let value = e.target.value;
+  let name = e.target.options[e.target.selectedIndex].textContent;
+  toImg.src = `https://flagsapi.com/${name}/flat/64.png`;
+  convertCurrency();
+});
+async function convertCurrency() {
+  let currencyName = fromCurrency.value;
+  let requiredCurrencyName = toCurrency.value;
+  const URL = `https://v6.exchangerate-api.com/v6/92b934d0119f8abaf3f99409/latest/${currencyName}`;
+  async function fetchData() {
+    let response = await fetch(URL);
+    let data = await response.json();
+
+    let exchangeRate = data.conversion_rates[requiredCurrencyName];
+    return exchangeRate;
+  }
+  let rate = await fetchData();
+  console.log(rate);
+}
